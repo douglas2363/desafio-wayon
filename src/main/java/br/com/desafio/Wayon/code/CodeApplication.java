bfedddbaff19b8010bba58cfec901ce92873e05e
Douglas <douglaslima2363@gmail.com>
2025-05-08 00:40:36 -0300

    tirando comentario para teste do application

--- src/main/java/br/com/desafio/Wayon/code/CodeApplication.java
+++ src/main/java/br/com/desafio/Wayon/code/CodeApplication.java
@@ -16,33 +16,33 @@ public class CodeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CodeApplication.class, args);
 
-//		// Criando a implementação concreta do ThirdPartyEmailSender
-//		ThirdPartyEmailSender sender = new ThirdPartyEmailSender() {
-//			@Override
-//			public void sendEmail(String to, String subject, String body) {
-//				// Implementação simulada ou real do serviço de e-mail
-//				System.out.println("Enviando e-mail para: " + to);
-//				System.out.println("Assunto: " + subject);
-//				System.out.println("Corpo: " + body);
-//			}
-//		};
-//
-//		// Criando o adaptador com a implementação do sender
-//		EmailService emailService = new ThirdPartyEmailAdapter(sender);
-//
-//		// Enviando o e-mail
-//		emailService.sendEmail("cliente@exemplo.com", "Bem-vindo", "Obrigado por se cadastrar!");
-//
-//		Pessoa p1 =  new Pessoa("João", "123456789");
-//		Pessoa p2 =  new Pessoa("Maria", "123456789");
-//
-//		System.out.println(p1.equals(p2)); // true, pois os CPFs são iguais
-//
-//		Set<Pessoa> pessoas = new HashSet<>();
-//		pessoas.add(p1);
-//		pessoas.add(p2); // Não será adicionado, pois o CPF já existe no conjunto
-//
-//		System.out.println(pessoas.size()); // 1, pois o CPF é a chave de comparação
+		// Criando a implementação concreta do ThirdPartyEmailSender
+		ThirdPartyEmailSender sender = new ThirdPartyEmailSender() {
+			@Override
+			public void sendEmail(String to, String subject, String body) {
+				// Implementação simulada ou real do serviço de e-mail
+				System.out.println("Enviando e-mail para: " + to);
+				System.out.println("Assunto: " + subject);
+				System.out.println("Corpo: " + body);
+			}
+		};
+
+		// Criando o adaptador com a implementação do sender
+		EmailService emailService = new ThirdPartyEmailAdapter(sender);
+
+		// Enviando o e-mail
+		emailService.sendEmail("cliente@exemplo.com", "Bem-vindo", "Obrigado por se cadastrar!");
+
+		Pessoa p1 =  new Pessoa("João", "123456789");
+		Pessoa p2 =  new Pessoa("Maria", "123456789");
+
+		System.out.println(p1.equals(p2)); // true, pois os CPFs são iguais
+
+		Set<Pessoa> pessoas = new HashSet<>();
+		pessoas.add(p1);
+		pessoas.add(p2); // Não será adicionado, pois o CPF já existe no conjunto
+
+		System.out.println(pessoas.size()); // 1, pois o CPF é a chave de comparação
 	}
 
 }
